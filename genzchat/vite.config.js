1d63b9a03d0e69509cb3f708b7a6809b27f48fe0
Yatharth Deoly <yatharthdeoly@gmail.com>
2025-09-09 16:07:16 +0530

    Updated configs | Added dockerFile

--- genzchat/vite.config.js
+++ genzchat/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-})
+  plugins: [tailwindcss(), react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://genzchat-backend:8088",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
